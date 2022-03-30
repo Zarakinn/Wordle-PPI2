3c05ebdb9716505d87c54c239bee0c3080549c43
Matéo DEMANGEON <mateo.demangeon@telecomnancy.eu>
2022-03-30 22:19:33 +0200

    🔨 IMPROVE: Possibilité d'encadrer le mot choisi par des fréquences

--- app_wordle/project/database/dict_tools.py
+++ app_wordle/project/database/dict_tools.py
@@ -12,12 +12,11 @@ def est_dans_dict(mot: str):
     return x[0][0] == 1
 
 
-def get_random_word(longueur: int, frequencemax=1000):
-    # Renvoie un mot aléatoire
-
-    words = basic_query("SELECT mot FROM dictionnaire WHERE longueur = ?",
-                        (longueur,), disable_dict_factory=True)
-
-    index = randrange(0, len(words))
-
-    return words[index][0]
+# Renvoie un mot aléatoire
+def get_random_word(longueur: int, frequencemin=0, frequencemax=20000):
+    try:
+        words = basic_query("SELECT mot FROM dictionnaire WHERE longueur = ? AND frequence >= ? AND frequence <= ?",
+                            (longueur, frequencemin, frequencemax,), disable_dict_factory=True)
+        return words[randrange(0, len(words))][0]
+    except ValueError:
+        print("Aucun mot ne correspond aux critères de recherche")
