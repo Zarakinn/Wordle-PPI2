c3ce3249a5550e1adb78a5f9c706d6eb2475b491
Matéo DEMANGEON <mateo.demangeon@telecomnancy.eu>
2022-04-02 19:05:09 +0200

    🔨 IMPROVE: Clavier interactif

--- app_wordle/project/static/js/Jeu.js
+++ app_wordle/project/static/js/Jeu.js
@@ -1,13 +1,23 @@
-window.onload = function (){
-    // the code to be called when the dom has loaded
-    // #document has its nodes
+window.onload = function () {
     const table = document.getElementById('table');
+    const clavier = document.getElementById('clavier');
     const nb_essais = table.rows.length;
     const nb_lettres = table.rows[0].cells.length;
-    jeu(nb_essais, nb_lettres);
-}
 
-function jeu(nb_essais, nb_lettres){
-    console.log(nb_essais);
-    console.log(nb_lettres);
+    document.addEventListener("keydown", (e) => {
+        press_key(e);
+    });
+
+    document.addEventListener("keyup", (e) => {
+        release_key(e);
+    });
+
+    function press_key(value) {
+        // Declenchee lors d'un clic sur le clavier ou sur une touche du clavier
+        document.getElementById(value.key.toUpperCase()).style.backgroundColor='#003F87';
+    }
+
+    function release_key(value) {
+        document.getElementById(value.key.toUpperCase()).style.backgroundColor='#25C9C973';
+    }
 }
