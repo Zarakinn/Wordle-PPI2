4a89029faf071a2a08295e1716cd5996e2a4a77b
Valentin <valentin.chanel@telecomnancy.eu>
2022-05-11 11:59:21 +0200

    ✨ NEW : fct list tools

--- solveur/src/solver/list_tools.h
+++ solveur/src/solver/list_tools.h
@@ -1,6 +1,7 @@
 //
 // Created by nicol on 09/05/2022.
 //
+#include <stdlib.h>
 
 #ifndef SOLVEUR_LIST_TOOLS_H
 #define SOLVEUR_LIST_TOOLS_H
@@ -12,14 +13,20 @@ typedef struct list_tries_t {
 
 typedef struct try_t {
     char* words;
-    int* results;
+    int* results; //ref a tableau du résultat du match
     struct try_t *next;
 } try_t;
 
 list_tries_t *create_list_tries();
+
 void destroy_list_tries(list_tries_t *list_tries);
+
 void append_list_tries(list_tries_t *list_tries, char *words, int *results);
+
+bool is_empty_list_tries(list_tries_t* list_tries);
+
 try_t *get_list_tries(list_tries_t *list_tries, int index);
+
 void display_list_tries(list_tries_t *list_tries);
 
 #endif //SOLVEUR_LIST_TOOLS_H
