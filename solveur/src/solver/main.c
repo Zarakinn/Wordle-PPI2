0af73025f3eb68463c7b86085e0f271532441433
Matéo DEMANGEON <mateo.demangeon@telecomnancy.eu>
2022-06-05 11:27:54 +0200

    🔨 IMPROVE: Les premiers mots sont maintenant inscrits en dur

--- solveur/src/solver/main.c
+++ solveur/src/solver/main.c
@@ -19,12 +19,21 @@ int main() {
     print_hello();
 
     int word_size = read_word_length();
+    if (word_size < 3 || word_size > 7) {printf("Longueur sélectionnée invalide.\n"); return 0;}
     import_dict(word_size);
     init_previous_attempts(word_size);
     list_attempts_t *previous_attempts = get_previous_attempt();
 
     //char *best_attempt = compute_next_best_attempt(word_size);
     //printf("%s", best_attempt);
+
+    // Sélection du premier mot
+    char *first_words[5] = {"rai", "aria", "aerer", "errera", "recrire"};
+    char *word_to_try = first_words[word_size - 3];
+    int *result = ask_for_answer(word_to_try);
+    append_attempt(previous_attempts, word_to_try, result);
+    update_current_possible_with_attempt();
+    printf("Il reste %d mots possibles", get_current_possible()->nb_words);
 
     while(get_current_possible()->nb_words > 1){
         char *word_to_try = compute_next_best_attempt();
@@ -33,7 +42,7 @@ int main() {
         update_current_possible_with_attempt();
         printf("Il reste %d mots possibles", get_current_possible()->nb_words);
     }
-    printf("La solution est : %s", get_current_possible()->head->word);
+    printf("\nLa solution est : %s\n", get_current_possible()->head->word);
 
     return 0;
 }
